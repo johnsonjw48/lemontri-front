158046f48483e8fcf1ed4ae9eeaac97cd3a2cf97
Stevens <sdemervil@gmail.com>
2022-12-13 14:51:33 +0100

    feat: add getQuizzes() + getQuiz()

--- src/app/services/strapi.service.ts
+++ src/app/services/strapi.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StrapiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private apiUrl: string = environment.apiUrl;
 
+  getQuizzes(): Observable<any> {
+    return this.http.get(this.apiUrl + '/quizzes');
+  }
 
+  getQuiz(id: number): Observable<any> {
+    return this.http.get(this.apiUrl + `/quizzes/${id}`);
+  }
 }
