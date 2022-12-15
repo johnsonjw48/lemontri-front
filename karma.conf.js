9779da6dc7e9058611b725027c7bb0a2ebdb661f
johnsonjw48 <73818994+johnsonjw48@users.noreply.github.com>
2022-12-15 15:43:53 +0100

    add scan project from features/scan

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/lemontri-front'),
+      dir: require('path').join(__dirname, './coverage/mobile-scanning-demo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule, SwRegistrationOptions } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartItemComponent } from './components/scanned-item/scanned-item.component';
+
+import { environment } from '../environments/environment';
+import {HttpClientModule} from "@angular/common/http";
+import { ScannerComponent } from './components/scanner/scanner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartItemComponent,
+    ScannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+
+    // PWA support
+    ServiceWorkerModule.register('ngsw-worker.js')
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SwRegistrationOptions,
+      useFactory: () => {
+        return {
+          enabled: environment.production,
+          registrationStrategy: 'registerImmediately'
+        };
+      }
+    },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
